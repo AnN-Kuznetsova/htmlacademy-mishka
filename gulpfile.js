97b453b806c6a8cb509a8dff29b3771312c65aaa
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-10-15 13:00:28 +0300

    Автоматизация: минифицирует CSS.

--- gulpfile.js
+++ gulpfile.js
@@ -11,15 +11,17 @@ const server = require("browser-sync").create();
 const imagemin = require("gulp-imagemin");
 const imageminJpegtran = require("imagemin-jpegtran");
 const webp = require("gulp-webp");
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
