c4ed46892f2b963d6493b0b44efeceacdc19f18d
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-10-15 19:32:25 +0300

    Починит фоновые svg.

--- gulpfile.js
+++ gulpfile.js
@@ -59,7 +59,7 @@ gulp.task("server", function () {
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/js/**/*.js", gulp.series("js", "refresh"));
-  gulp.watch("source/img/**/*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/img/**/*.svg", gulp.series("copy", "images", "sprite", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
